61fc36544b5fd730e4e4d6a08d0452d2c35388f5
Ольга <lelya.zy@gmail.com>
2023-08-14 19:29:48 +0300

    home work 1

--- hw_1/task_3.py
+++ hw_1/task_3.py
@@ -3,3 +3,22 @@
 # числа используйте код:
 # from random import randint
 # num = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+from random import randint
+ATTEMPT_LIMIT = 10
+target = int(randint(0, 100))
+num = None
+print(f'Подсказка, ответ: {target}')
+
+for attemp in range(ATTEMPT_LIMIT) :
+    num = int(input(f'Попытка № {attemp + 1} из {ATTEMPT_LIMIT}, введите число: '))
+    if target > num:
+        print('Загаданное число больше вашего')
+    elif target < num:
+        print('Загаданное число меньше вашего')
+    else:
+        print(f'Поздравляю, это {num} верный ответ!')
+        break
+else:
+    print(f'Все попытки исчерпаны, было загадано число {target}')
+    quit()
