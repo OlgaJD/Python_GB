d02e2cb623f2b81dbe13bf6054acab00997fe34f
Ольга <lelya.zy@gmail.com>
2023-09-08 13:19:05 +0300

    init

---
+++ lesson_7/file_worker/__init__.py
@@ -0,0 +1,6 @@
+from .hw_7_creator_demo import setting
+from .hw_7_file_sorter import file_sorter as sorter
+from .hw_7_file_renamer import file_renamer as renamer
+
+
+__all__ = ['setting', 'sorter', 'renamer']
